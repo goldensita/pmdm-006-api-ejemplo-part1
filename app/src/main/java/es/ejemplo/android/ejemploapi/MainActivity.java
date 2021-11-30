5cfdd254cb92d0bf4e811371c148764975d1accc
Daniel Garrido <daniel.garrido@grupstucom.com>
2021-11-30 11:31:36 +0100

    primera version de llamada a API

--- app/src/main/java/es/ejemplo/android/ejemploapi/MainActivity.java
+++ app/src/main/java/es/ejemplo/android/ejemploapi/MainActivity.java
@@ -13,9 +13,47 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
+             } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.i("JSON", s);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        task.execute("https://free.currconv.com/api/v7/convert?q=EUR_USD&compact=ultra&apiKey=7e8181340fcb67eff82f");
     }
 }
