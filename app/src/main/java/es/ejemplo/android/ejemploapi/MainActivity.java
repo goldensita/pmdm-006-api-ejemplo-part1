89513b037f0f4004e25779f8259613c32eca8898
Daniel Garrido <daniel.garrido@grupstucom.com>
2021-11-30 11:30:38 +0100

    first commit

---
+++ app/src/main/java/es/ejemplo/android/ejemploapi/MainActivity.java
@@ -0,0 +1,21 @@
+package es.ejemplo.android.ejemploapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
